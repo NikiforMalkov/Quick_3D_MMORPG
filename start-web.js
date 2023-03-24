ec4795b8e0b1fed30f75e810ae6b811772e82129
Nikifor <nikiformalkov@gmail.com>
2023-03-24 18:52:12 +0600

    fix start web js

--- start-web.js
+++ start-web.js
@@ -2,16 +2,26 @@ var express = require("express")
 
 let server = express()
 
-//https://web.dev/coop-coep/
-//https://expressjs.com/en/api.html
 server.use(
-  express.static('src/client', {
+    express.static('dist.browser/client', {
     setHeaders: function (res, path, stat) {
       res.set('Cross-Origin-Embedder-Policy', 'require-corp')
       res.set('Cross-Origin-Opener-Policy', 'same-origin')
     }
-  })
+    }),
 )
+server.use('/three', express.static('node_modules/three', {
+    setHeaders: function (res, path, stat) {
+        res.set('Cross-Origin-Embedder-Policy', 'require-corp')
+        res.set('Cross-Origin-Opener-Policy', 'same-origin')
+    }
+}))
+server.use('/socket.io-client', express.static('node_modules/socket.io-client/dist', {
+    setHeaders: function (res, path, stat) {
+        res.set('Cross-Origin-Embedder-Policy', 'require-corp')
+        res.set('Cross-Origin-Opener-Policy', 'same-origin')
+    }
+}))
 
 server.listen(5000)
 console.log('serving content on http://localhost:5000')
